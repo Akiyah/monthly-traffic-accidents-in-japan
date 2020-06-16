7bda2d5e298de3da0d08674e027a02dc9ea4bbc6
akiya.mizukoshi <akiya.mizukoshi@naviplus.co.jp>
2020-06-17 02:12:26 +0900

    create 2020/5

---
+++ create.rb
@@ -0,0 +1,106 @@
+require 'roo'
+require 'csv'
+require 'date'
+require 'open-uri'
+
+PARAMS = [
+  { year: 2020, month: 5, url: 'https://www.e-stat.go.jp/stat-search/file-download?statInfId=000031954930&fileKind=0', sheet: '表4-1' }
+]
+
+AREA_PREFECTURE = [
+  nil, nil, nil, nil, nil,
+
+  ["北海道","札幌"],
+  ["北海道","函館"],
+  ["北海道","旭川"],
+  ["北海道","釧路"],
+  ["北海道","北見"],
+  nil,
+
+  ["東北","青森"],
+  ["東北","岩手"],
+  ["東北","宮城"],
+  ["東北","秋田"],
+  ["東北","山形"],
+  ["東北","福島"],
+  nil,
+
+  ["東京","東京"],
+
+  ["関東","茨城"],
+  ["関東","栃木"],
+  ["関東","群馬"],
+  ["関東","埼玉"],
+  ["関東","千葉"],
+  ["関東","神奈川"],
+  ["関東","新潟"],
+  ["関東","山梨"],
+  ["関東","長野"],
+  ["関東","静岡"],
+  nil,
+
+  ["中部","富山"],
+  ["中部","石川"],
+  ["中部","福井"],
+  ["中部","岐阜"],
+  ["中部","愛知"],
+  ["中部","三重"],
+  nil,
+
+  ["近畿","滋賀"],
+  ["近畿","京都"],
+  ["近畿","大阪"],
+  ["近畿","兵庫"],
+  ["近畿","奈良"],
+  ["近畿","和歌山"],
+  nil,
+
+  ["中国","鳥取"],
+  ["中国","島根"],
+  ["中国","岡山"],
+  ["中国","広島"],
+  ["中国","山口"],
+  nil,
+
+  ["四国","徳島"],
+  ["四国","香川"],
+  ["四国","愛媛"],
+  ["四国","高知"],
+  nil,
+
+  ["九州","福岡"],
+  ["九州","佐賀"],
+  ["九州","長崎"],
+  ["九州","熊本"],
+  ["九州","大分"],
+  ["九州","宮崎"],
+  ["九州","鹿児島"],
+  ["九州","沖縄"],
+  nil,
+
+  nil,
+]
+
+puts %w(year month area prefecture 発生件数（速報値） 死者数（確定値） 負傷者数（速報値）).join("\t")
+
+PARAMS.each do |param|
+  year = param[:year]
+  month = param[:month]
+  sheet = param[:sheet]
+  url = param[:url]
+
+  open(url) do |file|
+    xlsx = Roo::Excelx.new(file)
+
+    AREA_PREFECTURE.each_with_index do |area_prefecture, i|
+      next unless area_prefecture
+      area, prefecture = area_prefecture
+
+      v0 = xlsx.sheet(sheet).cell(i + 1, 'C')
+      v1 = xlsx.sheet(sheet).cell(i + 1, 'F')
+      v2 = xlsx.sheet(sheet).cell(i + 1, 'J')
+      puts [year, month, area, prefecture, v0, v1, v2].join("\t")
+    end
+  end
+end
+
