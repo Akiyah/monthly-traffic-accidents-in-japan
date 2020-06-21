febd1d99dce1358187345d218780f323aeb7453f
akiya.mizukoshi <akiya.mizukoshi@naviplus.co.jp>
2020-06-22 00:13:13 +0900

    add '月末'

--- create.rb
+++ create.rb
@@ -24,17 +24,16 @@ PARAMS.each do |param|
 end
 
 CSV.open('tsv/monthly-traffic-accidents-in-japan.tsv','w', col_sep: "\t") do |tsv|
-  #tsv << %w(年 月 管区 都道府県 発生件数（速報値） 死者数（確定値） 負傷者数（速報値） 発生件数（速報値）月末 死者数（確定値）月末 負傷者数（速報値）月末)
-  tsv << %w(年 月 管区 都道府県 発生件数（速報値） 死者数（確定値） 負傷者数（速報値）)
+  tsv << %w(年 月 管区 都道府県 発生件数（速報値） 死者数（確定値） 負傷者数（速報値） 発生件数（速報値）月末 死者数（確定値）月末 負傷者数（速報値）月末)
   data.each do |year, _|
     data[year].each do |month, _|
       data[year][month].each do |area, _|
         data[year][month][area].each do |prefecture, v|
           if v[:v0] && v[:v1] && v[:v2]
-            tsv << [year, month, area, prefecture, v[:v0], v[:v1], v[:v2]]
+            tsv << [year, month, area, prefecture, v[:v0], v[:v1], v[:v2], v[:v3], v[:v4], v[:v5]]
           elsif data[year][month - 1]
             w = data[year][month - 1][area][prefecture]
-            tsv << [year, month, area, prefecture, v[:v3] - w[:v3], v[:v4] - w[:v4], v[:v5] - w[:v5]]
+            tsv << [year, month, area, prefecture, v[:v3] - w[:v3], v[:v4] - w[:v4], v[:v5] - w[:v5], v[:v3], v[:v4], v[:v5]]
           end
         end
       end
