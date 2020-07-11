088b2adcbb1f6a849babbdd615cae134629b8fa1
akiya.mizukoshi <akiya.mizukoshi@naviplus.co.jp>
2020-07-11 18:39:15 +0900

    refactoring: wip

--- compared_measures.rb
+++ compared_measures.rb
@@ -36,23 +36,15 @@ class ComparedMeasures
 
   def self.create_from_block
     a = [
-      [:measures, :v0],
-      [:measures, :v1],
-      [:measures, :v2],
-      [:measures_in_year, :v0],
-      [:measures_in_year, :v1],
-      [:measures_in_year, :v2],
-      [:measures_change, :v0],
-      [:measures_change, :v1],
-      [:measures_change, :v2],
-      [:measures_change_in_year, :v0],
-      [:measures_change_in_year, :v1],
-      [:measures_change_in_year, :v2]
-    ].map do |measures_key, v_key|
-      #[:v0, :v1, :v2].map do |k|
+      :measures,
+      :measures_in_year,
+      :measures_change,
+      :measures_change_in_year,
+    ].map do |measures_key|
+      [:v0, :v1, :v2].map do |v_key|
         yield(measures_key, v_key)
-      #end
-    end
+      end
+    end.flatten
 
     ComparedMeasures.new(*a)
   end
@@ -79,6 +71,17 @@ class ComparedMeasures
     @measures_in_year = Measures.new(v3, v4, v5)
     @measures_change = Measures.new(v0_, v1_, v2_)
     @measures_change_in_year = Measures.new(v3_, v4_, v5_)
+  end
+
+  def self.create_by_measures(measures, measures_in_year, measures_change = Measures.new(nil, nil, nil), measures_change_in_year = Measures.new(nil, nil, nil))
+    cm = ComparedMeasures.new(nil, nil, nil, nil, nil, nil)
+    cm.measures = measures
+    cm.measures_in_year = measures_in_year
+    cm.measures_change = measures_change
+    cm.measures_change_in_year = measures_change_in_year
+
+    cm.initialize_vs
+    cm
   end
 
   def initialize_vs
@@ -131,12 +134,16 @@ class ComparedMeasures
   def diff012345to_
     return nil if @measures_change.empty? || @measures_change_in_year.empty?
 
-    a = @measures.diff(@measures_change).to_a + @measures_in_year.diff(@measures_change_in_year).to_a
-    ComparedMeasures.new(*a)
+    ComparedMeasures.create_by_measures(
+      @measures.diff(@measures_change),
+      @measures_in_year.diff(@measures_change_in_year)
+    )
   end
 
   def create_next_month_from_next_next_month(cm2)
-    a = cm2.measures_in_year.diff(cm2.measures).diff(measures_in_year).to_a + cm2.measures_in_year.diff(cm2.measures).to_a
-    ComparedMeasures.new(*a)
+    ComparedMeasures.create_by_measures(
+      cm2.measures_in_year.diff(cm2.measures).diff(measures_in_year),
+      cm2.measures_in_year.diff(cm2.measures)
+    )
   end
 end
