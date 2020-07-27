d3226f38240b9ffa1c99a02f004fb8c4408ab496
akiya.mizukoshi <akiya.mizukoshi@naviplus.co.jp>
2020-07-28 02:16:46 +0900

    add test for fill

--- spec/year_month_area_prefecture_data_spec.rb
+++ spec/year_month_area_prefecture_data_spec.rb
@@ -104,6 +104,55 @@ RSpec.describe YearMonthAreaPrefectureData do
     end
   end
 
+  context "#fill" do
+    let(:cm_2019_5) { ComparedMeasures.create_from_a([20, 10, 30, 20*5, 10*5, 30*5, 4, 2, 6, 4*5, 2*5, 6*5]) }
+    let(:cm_2019_6) { ComparedMeasures.create_from_a([nil, nil, nil, 20*6, 10*6, 30*6, nil, nil, nil, 4*6, 2*6, 6*6]) }
+    let(:cm_2019_7) { ComparedMeasures.create_from_a([20, 10, 30, 20*7, 10*7, 30*7, 4, 2, 6, 4*7, 2*7, 6*7]) }
+    let(:cm_2019_9) { ComparedMeasures.create_from_a([20, 10, 30, 20*9, 10*9, 30*9, 4, 2, 6, 4*9, 2*9, 6*9]) }
+
+    before do
+      data.set(2019, 5, 'area1', 'prefecture1', cm_2019_5)
+      data.set(2019, 6, 'area1', 'prefecture1', cm_2019_6)
+      data.set(2019, 7, 'area1', 'prefecture1', cm_2019_7)
+      data.set(2019, 9, 'area1', 'prefecture1', cm_2019_9)
+    end
+
+    let(:expected) do
+      [
+        [2019, 5, "area1", "prefecture1", [20, 10, 30, 20*5, 10*5, 30*5, 4, 2, 6, 4*5, 2*5, 6*5]],
+        [2019, 5, "area1", "計",          [20, 10, 30, 20*5, 10*5, 30*5, 4, 2, 6, 4*5, 2*5, 6*5]],
+        [2019, 6, "area1", "prefecture1", [20, 10, 30, 20*6, 10*6, 30*6, 4, 2, 6, 4*6, 2*6, 6*6]],
+        [2019, 6, "area1", "計",          [20, 10, 30, 20*6, 10*6, 30*6, 4, 2, 6, 4*6, 2*6, 6*6]],
+        [2019, 7, "area1", "prefecture1", [20, 10, 30, 20*7, 10*7, 30*7, 4, 2, 6, 4*7, 2*7, 6*7]],
+        [2019, 7, "area1", "計",          [20, 10, 30, 20*7, 10*7, 30*7, 4, 2, 6, 4*7, 2*7, 6*7]],
+        [2019, 9, "area1", "prefecture1", [20, 10, 30, 20*9, 10*9, 30*9, 4, 2, 6, 4*9, 2*9, 6*9]],
+        [2019, 9, "area1", "計",          [20, 10, 30, 20*9, 10*9, 30*9, 4, 2, 6, 4*9, 2*9, 6*9]],
+        [2019, 8, "area1", "prefecture1", [20, 10, 30, 20*8, 10*8, 30*8, nil, nil, nil, nil, nil, nil]],
+        [2019, 8, "area1", "計",          [20, 10, 30, 20*8, 10*8, 30*8, nil, nil, nil, nil, nil, nil]],
+        [2018, 5, "area1", "prefecture1", [16, 8, 24, 80, 40, 120, nil, nil, nil, nil, nil, nil]],
+        [2018, 5, "area1", "計",          [16, 8, 24, 80, 40, 120, nil, nil, nil, nil, nil, nil]],
+        [2018, 6, "area1", "prefecture1", [16, 8, 24, 96, 48, 144, nil, nil, nil, nil, nil, nil]],
+        [2018, 6, "area1", "計",          [16, 8, 24, 96, 48, 144, nil, nil, nil, nil, nil, nil]],
+        [2018, 7, "area1", "prefecture1", [16, 8, 24, 112, 56, 168, nil, nil, nil, nil, nil, nil]],
+        [2018, 7, "area1", "計",          [16, 8, 24, 112, 56, 168, nil, nil, nil, nil, nil, nil]],
+        [2018, 9, "area1", "prefecture1", [16, 8, 24, 144, 72, 216, nil, nil, nil, nil, nil, nil]],
+        [2018, 9, "area1", "計",          [16, 8, 24, 144, 72, 216, nil, nil, nil, nil, nil, nil]],
+        [2018, 8, "area1", "prefecture1", [16, 8, 24, 128, 64, 192, nil, nil, nil, nil, nil, nil]],
+        [2018, 8, "area1", "計",          [16, 8, 24, 128, 64, 192, nil, nil, nil, nil, nil, nil]]
+      ]
+    end
+
+    it do
+      data.fill
+
+      i = 0
+      data.each do |year, month, area, prefecture, cm|
+        expect([year, month, area, prefecture, cm.to_a]).to eq expected[i]
+        i += 1
+      end
+    end
+  end
+
   context "#write" do
     let(:cm1) { ComparedMeasures.create_from_a((1..12).to_a) }
     let(:cm2) { ComparedMeasures.create_from_a((2..13).to_a) }
