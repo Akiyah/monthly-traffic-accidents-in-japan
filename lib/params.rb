fd3e203a996e720e3c60e3a8cfbee566df3cd51e
akiya.mizukoshi <akiya.mizukoshi@naviplus.co.jp>
2020-09-11 03:21:23 +0900

    add 2020/08

--- lib/params.rb
+++ lib/params.rb
@@ -1,4 +1,5 @@
 PARAMS = [
+  { year: 2020, month:  8, url: 'https://www.e-stat.go.jp/stat-search/file-download?statInfId=000031978927&fileKind=0', format: 'A1', format2: 'A1' },
   { year: 2020, month:  7, url: 'https://www.e-stat.go.jp/stat-search/file-download?statInfId=000031973240&fileKind=0', format: 'A1', format2: 'A1' },
   { year: 2020, month:  6, url: 'https://www.e-stat.go.jp/stat-search/file-download?statInfId=000031964076&fileKind=0', format: 'A1', format2: 'A1' },
   { year: 2020, month:  5, url: 'https://www.e-stat.go.jp/stat-search/file-download?statInfId=000031954930&fileKind=0', format: 'A1', format2: 'A1' },
